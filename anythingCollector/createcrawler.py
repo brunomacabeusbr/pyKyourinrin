7c419579abbbec687b19251a676076feea839c00
Bruno Macabeus <bruno.macabeus@gmail.com>
2015-10-18 01:09:07 -0300

    You can now use compound names, like "portal transparencia" Fixbug: Sometimes it can't get the directory path correctly

--- anythingCollector/createcrawler.py
+++ anythingCollector/createcrawler.py
@@ -1,17 +1,18 @@
 import sys
 import os
 
-
-my_path = os.path.dirname(__file__)
+my_path = os.path.dirname(os.path.realpath(__file__))
 crawler_folder = my_path + '/crawler/'
-crawler_name = sys.argv[1]
+crawler_name_array = sys.argv[1:]
+crawler_name_underscore = '_'.join(crawler_name_array)
+crawler_name_camel_case = ''.join(i.title() for i in crawler_name_array)
 
-with open(crawler_folder + crawler_name + '.py', 'x') as f:
+with open(crawler_folder + crawler_name_underscore + '.py', 'x') as f:
     content = \
 """from . import Crawler
 
 
-class Crawler""" + crawler_name.capitalize() + """(Crawler):
+class Crawler""" + crawler_name_camel_case + """(Crawler):
     def create_my_table(self):
         self.db.execute('CREATE TABLE IF NOT EXISTS %s('
                             'peopleid INTEGER,'
@@ -21,7 +22,7 @@ class Crawler""" + crawler_name.capitalize() + """(Crawler):
 
     @staticmethod
     def name():
-        return '""" + crawler_name + """'
+        return '""" + crawler_name_underscore + """'
 
     @staticmethod
     def dependencies():
