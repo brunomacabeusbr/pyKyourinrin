cfe664f757c4c18b4b682c7816bf4e8fcd4dd1e8
Bruno Macabeus <bruno.macabeus@gmail.com>
2016-02-21 17:42:51 -0300

    New crawler for primitive news: aylien_summarize

---
+++ pyKyourinrin/crawler/aylien_summarize.py
@@ -0,0 +1,60 @@
+from . import Crawler
+from aylienapiclient import textapi
+
+
+class CrawlerAylienSummarize(Crawler):
+    def create_my_table(self):
+        self.db.execute('CREATE TABLE IF NOT EXISTS %s('
+                            'primitive_news_id INTEGER'
+                        ');' % self.name())
+
+        self.db.execute('CREATE TABLE IF NOT EXISTS %s('
+                            'primitive_news_id INTEGER,'
+                            'text TEXT'
+                        ');' % (self.name() + '_sentences'))
+
+    @staticmethod
+    def read_my_secondary_tables():
+        return (
+            {'table': 'sentences'},
+        )
+
+    @staticmethod
+    def column_export():
+        def key_sentences(read):
+            if len(read['sentences']) > 0:
+                return read['sentences']
+            else:
+                return None
+
+        return (
+            {'column_name': 'key_sentences', 'how': key_sentences},
+        )
+    @staticmethod
+    def name():
+        return 'aylien_summarize'
+
+    @staticmethod
+    def dependencies():
+        return 'title', 'article'
+
+    @staticmethod
+    def crop():
+        return 'key_sentences',
+
+    @staticmethod
+    def primitive_required():
+        return 'primitive_news',
+
+    @classmethod
+    def harvest(cls, primitive_news=None, dependencies=None, total_sentences=5):
+        if primitive_news is None:
+            raise ValueError('Você obrigatorialmente deve fornecer um id de primitive news!')
+
+        client = textapi.Client('71085b2c', '9a69067df3c7f538060fedac9c1adbc0')
+        summary = client.Summarize({'title': dependencies['title'], 'text': dependencies['article'], 'sentences_number': total_sentences})
+
+        cls.update_my_table({})
+        for i in summary['sentences']:
+            cls.update_my_table({'text': i}, table='sentences')
+        cls.update_crawler_status(True)
