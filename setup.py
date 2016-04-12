acfa0e61aed47dc94906adb386d194f1eaee0dcf
brunomacabeusbr <bruno.macabeus@gmail.com>
2016-04-12 16:01:54 -0300

    Update setup url

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='spyck',
+    version='0.0.2',
+    description='Crawler framework',
+    author='Bruno Macabeus',
+    url='http://macalogs.com.br/spyck-apresentacao/',
+    download_url='https://github.com/zetaresearch/spyck',
+    keywords=['crawler', 'framework'],
     install_requires=[
         'requests',
         'PyPDF2',
@@ -10,5 +16,6 @@ setup(
         'aylien-apiclient'
         # todo: precisa exigir o phantomjs
     ],
-    packages=['pyKyorinrin']
+    packages=['spyck'],
+    packages_data={'spyck': ['crawler_xml/etufor.xml']}
 )
